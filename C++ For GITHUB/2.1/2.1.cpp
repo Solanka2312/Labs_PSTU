18851e9a639fa20d53a2de8a1f784b9d56822dfb
Кирилл Рожков <kirillrozhkov97@gmail.com>
2025-11-25 14:05:52 +0500

    Add files via upload

---
+++ C++ For GITHUB/2.1/2.1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n; int sum = 0;
+    cin >> n;
+    if (n <= 0) {
+        cout << endl;
+    }
+    else {
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        cout << sum << endl;
+    }
+}
